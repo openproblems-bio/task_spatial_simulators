228f965eb929a9851e11cef9194ddb5215c62b1b
littlecabiria <71769896+littlecabiria@users.noreply.github.com>
2024-08-16 16:02:51 +1000

    add helper functions

--- src/helpers/utils.R
+++ src/helpers/utils.R
@@ -1,13 +1,25 @@
+# library(dplyr) need this package
+requireNamespace("dplyr", quietly = TRUE)
 # spatial autocorrelation
 generate_moransI <- function(adata){
   sp_count <- adata$layers[["logcounts"]]
-  
   W <- cbind.data.frame(row = as.numeric(sapply(strsplit(rownames(sp_count),split="x"),"[",1)),
                         col = as.numeric(sapply(strsplit(rownames(sp_count),split="x"),"[",2)))
   W <- 1/as.matrix(dist(W))
   diag(W) <- 0
   res <- spots::BivariateMoransI( X =  sp_count , W =  W)
   return(res$Morans.I)
+}
+
+# real <- generate_moransI(input_real_sp)
+# sim <- generate_moransI(input_simulated_sp)
+
+generate_cosine <- function(real, sim){
+  real_new <- real[!is.na(real) & !is.na(sim)]
+  sim_new <- sim[!is.na(real) & !is.na(sim)]
+  similarity <- cosine(as.textmatrix(cbind(as.vector(real_new), as.vector(sim_new))))
+  return(mean(similarity))
+
 }
 
 # Spatial varianble gene
@@ -28,10 +40,32 @@ generate_svg_sparkx <- function(adata) {
   return(sparkX)
 }
 
+calculate_precision <- function(real_svg, sim_svg) {
+  filtered_real_data <- real_svg$res_mtest %>% 
+    dplyr::filter(adjustedPval < 0.05)
+  filtered_compared_data <- sim_svg$res_mtest %>% 
+    dplyr::filter(adjustedPval < 0.05)
+  tp <- length(intersect(row.names(filtered_real_data), row.names(filtered_compared_data)))
+  fp <- length(setdiff(row.names(filtered_compared_data), row.names(filtered_real_data)))
+  precision <- ifelse((tp + fp) > 0, tp / (tp + fp), NA)
+  return(precision)
+}
+
+calculate_recall <- function(real_svg, sim_svg) {
+  filtered_real_data <- real_svg$res_mtest %>% 
+    filter(adjustedPval < 0.05)
+  filtered_compared_data <- sim_svg$res_mtest %>% 
+    filter(adjustedPval < 0.05)
+  tp <- length(intersect(row.names(filtered_real_data), row.names(filtered_compared_data)))
+  fn <- length(setdiff(row.names(filtered_real_data), row.names(filtered_compared_data)))
+  recall <- ifelse((tp + fn) > 0, tp / (tp + fn), NA)
+  return(recall)
+}
+
 # cell type deconvolution
 CARD_processing <- function(sp_adata, sc_adata){
-  sp_adata <- input_real_sp
-  sc_adata <- input_sc
+  # sp_adata <- input_real_sp
+  # sc_adata <- input_sc
   spatial_count <- Matrix::t(sp_adata$layers[["counts"]])
   spatial_location <- cbind.data.frame(
     x = as.numeric(sapply(strsplit(colnames(spatial_count),split="x"),"[",1)),
@@ -58,14 +92,37 @@ CARD_processing <- function(sp_adata, sc_adata){
 	  minCountSpot = 5) 
   
   CARD_obj <- CARD::CARD_deconvolution(CARD_object = CARD_obj)
-  
-  metadata(sp_obj)$celltype_prop <- CARD_obj@Proportion_CARD
-  
-  diff_list <- setdiff(colnames(counts(sp_obj)), rownames(sp_obj@metadata$celltype_prop))
-  current_colnames <- colnames(sp_obj)
-  columns_to_keep <- !current_colnames %in% diff_list
-  sp_obj <- sp_obj[, columns_to_keep]
 
-  return(sp_obj)
+  Proportion_CARD <- as.matrix(CARD_obj@Proportion_CARD)
+
+  return(Proportion_CARD)
+
 }
+
+generate_jds <- function(real, sim) {
+  common_row_names <- intersect(rownames(real), rownames(sim))
+  real_common <- real[common_row_names, , drop = FALSE]
+  sim_common <- sim[common_row_names, , drop = FALSE]
+  jsd_values <- sapply(1:nrow(real_common), function(i) {
+    x.count <- rbind(as.vector(real_common[i, ]), as.vector(sim_common[i, ]))
+    philentropy::JSD(x.count, est.prob = "empirical")
+  })
+  average_jsd <- mean(jsd_values)
+  return(average_jsd)
+}
+
+generate_rmse <- function(real, sim) {
+  common_row_names <- intersect(rownames(real), rownames(sim))
+  real_common <- real[common_row_names, , drop = FALSE]
+  sim_common <- sim[common_row_names, , drop = FALSE]
+  rmse_values <- sapply(1:nrow(real_common), function(i) {
+    Metrics::rmse(as.vector(real_common[i, ]), as.vector(sim_common[i, ]))
+  })
+  average_rmse <- mean(rmse_values)
+  return(average_rmse)
+}
+
+
+
 # spatial clustering
+
