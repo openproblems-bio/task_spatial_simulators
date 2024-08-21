552f9d01dba61aa7628871887dcd9648ff72c4fe
littlecabiria <71769896+littlecabiria@users.noreply.github.com>
2024-08-21 19:22:42 +1000

    add generate_sim_spatialCluster

--- src/metrics/downstream/script.R
+++ src/metrics/downstream/script.R
@@ -39,22 +39,23 @@ crosscor_mantel <- generate_mantel(real_moransI, sim_moransI)
 
 cat("spatial clustering evaluation\n")
 # TODO
-sim_sce <- scater::logNormCounts(SingleCellExperiment::SingleCellExperiment(
-  list(counts = Matrix::t(input_simulated_sp$layers[["counts"]])),
-  colData = input_simulated_sp$obs,
-  metadata = input_simulated_sp$obsm
-))
+# sim_sce <- scater::logNormCounts(SingleCellExperiment::SingleCellExperiment(
+#   list(counts = Matrix::t(input_simulated_sp$layers[["counts"]])),
+#   colData = input_simulated_sp$obs,
+#   metadata = input_simulated_sp$obsm
+# ))
 
 # generate the simulated clustering result first by BayersSpace
-sim_sce <- BayesSpace::spatialPreprocess(sim_sce, platform=par$plat, 
-                              n.PCs=7, n.HVGs=2000, log.normalize=FALSE)
-sim_sce <- BayesSpace::spatialCluster(sim_sce, q=max(unique(input_real_sp$obs[,c("spatial_cluster")])), platform=par$plat, d=7,
-                           init.method="mclust", model="t", gamma=2,
-                           nrep=1000, burn.in=100,
-                           save.chain=TRUE)
+# sim_sce <- BayesSpace::spatialPreprocess(sim_sce, platform=par$plat, 
+#                               n.PCs=7, n.HVGs=2000, log.normalize=FALSE)
+# sim_sce <- BayesSpace::spatialCluster(sim_sce, q=max(unique(input_real_sp$obs[,c("spatial_cluster")])), platform=par$plat, d=7,
+#                            init.method="mclust", model="t", gamma=2,
+#                            nrep=1000, burn.in=100,
+#                            save.chain=TRUE)
 # reclassify the clustering result
 real_cluster <- input_real_sp$obs[,c("spatial_cluster")]
-sim_cluster <- sim_sce$spatial.cluster
+# sim_cluster <- sim_sce$spatial.cluster
+sim_cluster <- generate_sim_spatialCluster(input_real_sp, input_simulated_sp)
 location <- colnames(counts(sim_sce))
 sim_new_cluster <- reclassify_simsce(location, real_cluster, sim_cluster)
 # ART and NMI
