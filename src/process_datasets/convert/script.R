90bcfbe4f4e4c133285e674f48346a0ea018c275
littlecabiria <71769896+littlecabiria@users.noreply.github.com>
2024-08-26 16:37:21 +1000

    Update script.R

--- src/process_datasets/convert/script.R
+++ src/process_datasets/convert/script.R
@@ -7,8 +7,8 @@ par <- list(
   input_sp = "resources_test/datasets_raw/MOBNEW/dataset_sp.rds",
 
   # outputs
-  output_sc = "resources_test/datasets/MOBNEW/dataset_sc.rds",
-  output_sp = "resources_test/datasets/MOBNEW/dataset_sp.rds",
+  output_sc = "resources_test/datasets/MOBNEW/dataset_sc.h5ad",
+  output_sp = "resources_test/datasets/MOBNEW/dataset_sp.h5ad",
 
   # dataset metadata
   dataset_id = "MOBNEW",
