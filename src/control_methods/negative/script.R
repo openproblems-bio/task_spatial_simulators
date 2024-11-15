342d3b531a6ac6a943ecda749ffa9eb94b311238
littlecabiria <71769896+littlecabiria@users.noreply.github.com>
2024-11-15 22:27:08 +1100

    update negative method

--- src/control_methods/negative/script.R
+++ src/control_methods/negative/script.R
@@ -1,7 +1,7 @@
 ## VIASH START
 par <- list(
-  input = "resources_test/datasets/MOBNEW/dataset_sp.h5ad",
-  output = "simulated_dataset.h5ad",
+  input = "temp_olfactorybulb.negative.ks_statistic_gene_cell/_viash_par/input_spatial_dataset_1/output_sp.h5ad",
+  output = "simulated_dataset.h5ad"
 )
 meta <- list(
   name = "negative"
@@ -13,11 +13,34 @@ input <- anndata::read_h5ad(par$input)
 
 count_matrix <- as.matrix(input$layers[['counts']])
 
+# Calculate the total number of elements
+total_elements <- length(count_matrix)
+
+# Count the number of zeros
+num_zeros <- sum(count_matrix == 0)
+
+# Calculate the proportion of zeros
+proportion_zeros <- num_zeros / total_elements
+
+# Print the result
+proportion_zeros
+
 n_rows <- nrow(count_matrix)
 n_cols <- ncol(count_matrix)
 
 random_matrix_uniform <- matrix(runif(n_rows * n_cols), nrow = n_rows, ncol = n_cols)
 rownames(random_matrix_uniform) <- rownames(count_matrix)
+
+# Calculate the number of elements to set to zero
+num_elements <- length(random_matrix_uniform)
+num_zeros <- ceiling(proportion_zeros * num_elements)
+
+# Randomly select indices to set to zero
+indices <- sample(seq_len(num_elements), num_zeros)
+
+# Set the selected indices to zero
+random_matrix_uniform[indices] <- 0
+
 
 cat("Generate outoput file\n")
 output <- anndata::AnnData(
