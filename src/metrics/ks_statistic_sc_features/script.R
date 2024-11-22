84f5809591ca127b2068fc3d332ace51cd54271b
littlecabiria <71769896+littlecabiria@users.noreply.github.com>
2024-11-22 15:45:35 +1100

    Update script.R

--- src/metrics/ks_statistic_sc_features/script.R
+++ src/metrics/ks_statistic_sc_features/script.R
@@ -8,13 +8,7 @@ requireNamespace("CARD", quietly = TRUE)
 requireNamespace("SingleCellExperiment", quietly = TRUE)
 requireNamespace("SummarizedExperiment", quietly = TRUE)
 
-# <<<<<<< Updated upstream
-## VIASH START
-par <- list(
-  input_spatial_dataset = "resources_test/spatialsimbench_mobnew/dataset_sp.h5ad",
-  input_singlecell_dataset = "resources_test/spatialsimbench_mobnew/dataset_sc.h5ad",
-  input_simulated_dataset = "resources_test/spatialsimbench_mobnew/simulated_dataset.h5ad",
-=======
+
 packages <- c("SingleCellExperiment", "SummarizedExperiment", "concaveman", "sp", 
               "Matrix", "methods", "ggplot2", "ggcorrplot", "MuSiC", "fields", 
               "MCMCpack", "dplyr", "sf", "RANN", "stats", "reshape2", "RColorBrewer", 
@@ -29,7 +23,6 @@ par <- list(
   input_spatial_dataset = "temp_pdac.positive.ks_statistic_sc_features/_viash_par/input_spatial_dataset_1/output_sp.h5ad",
   input_singlecell_dataset = "temp_pdac.positive.ks_statistic_sc_features/_viash_par/input_singlecell_dataset_1/output_sc.h5ad",
   input_simulated_dataset = "temp_pdac.positive.ks_statistic_sc_features/_viash_par/input_simulated_dataset_1/pdac.positive.generate_sim_spatialcluster.output_sp.h5ad",
->>>>>>> Stashed changes
   output = "output.h5ad"
 )
 meta <- list(
@@ -48,9 +41,7 @@ real_log_count <- t(input_real_sp$layers[["logcounts"]])
 real_prob_matrix <- input_real_sp$obsm[["celltype_proportions"]]
 colnames(real_prob_matrix) <- paste0("ct", seq_len(ncol(real_prob_matrix)))
 
-# <<<<<<< Updated upstream
 sim_log_count <- scuttle::normalizeCounts(t(input_simulated_sp$layers[["counts"]]))
-=======
 real_log_count <- real_log_count[  , colnames(real_log_count)  %in% rownames(real_prob_matrix) ]
 
 
@@ -61,7 +52,6 @@ sim_sce <- scater::logNormCounts(SingleCellExperiment::SingleCellExperiment(
 ))
 
 sim_log_count <- SummarizedExperiment::assay(sim_sce, "logcounts")
->>>>>>> Stashed changes
 
 # build cell type deconvolution in simulated data ## error
 sim_prob_matrix <- CARD_processing(input_real_sp, input_sc)
@@ -80,15 +70,6 @@ real_scfeatures_result <- scFeatures::scFeatures(real_log_count,
 )
 
 sim_scfeatures_result <- scFeatures::scFeatures(sim_log_count,
-<<<<<<< Updated upstream
-  sample = rep("sample1", ncol(sim_log_count)),
-  spatialCoords = input_simulated_sp$obs[c("row", "col")],
-  feature_types = feat_types,
-  type = "spatial_t",
-  species = sc_species,
-  spotProbability =  t(sim_prob_matrix)
-)
-=======
                                                 sample = rep("sample1", ncol(sim_log_count)),
                                                 spatialCoords = list( as.numeric( unlist( lapply( strsplit( colnames(sim_log_count) , "x"), `[`, 1) ) )    ,
                                                                       as.numeric( unlist( lapply( strsplit( colnames(sim_log_count) , "x"), `[`, 2) ) )  )  ,
@@ -96,7 +77,7 @@ sim_scfeatures_result <- scFeatures::scFeatures(sim_log_count,
                                                 type = "spatial_t",
                                                 species = sc_species,
                                                 spotProbability =  t(sim_prob_matrix) )
->>>>>>> Stashed changes
+
 
 ks_statistic_L_stats <- ks::kde.test(x1 = as.numeric(real_scfeatures_result$L_stats), x2 = as.numeric(sim_scfeatures_result$L_stats))
 ks_statistic_celltype_interaction <- ks::kde.test(x1 = as.numeric(real_scfeatures_result$celltype_interaction), x2 = as.numeric(sim_scfeatures_result$celltype_interaction))
